2c405292fb147364ff3e373fab5e183be310f203
likun <496546144@qq.com>
2019-04-24 17:06:33 +0800

    优化迁移到 AndroidX

--- app/src/main/java/com/ashlikun/zxing/simple/MainActivity.java
+++ app/src/main/java/com/ashlikun/zxing/simple/MainActivity.java
@@ -2,7 +2,7 @@ package com.ashlikun.zxing.simple;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.TextureView;
 import android.widget.ImageView;
